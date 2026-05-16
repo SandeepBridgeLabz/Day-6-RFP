87bc2e415a00cf786984aa59398058c70e221b7a
sandeep <sandeepmishra898k@gmail.com>
2026-05-16 15:17:57 +0530

    5. Coupon Numbers

---
+++ App/src/CouponNumbers.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
+public class CouponNumbers {
+
+    public static int generateCoupon(int n) {
+        Random random = new Random();
+        return random.nextInt(n);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int count = 0;
+
+        while(set.size() < n) {
+            int coupon = generateCoupon(n);
+            set.add(coupon);
+            count++;
+        }
+
+        System.out.println("Total Random Numbers Needed: " + count);
+    }
+}
